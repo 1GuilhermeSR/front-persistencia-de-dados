d5c385f68817fa9d8707bde9555699c4549d9579
Victor Caetano Souto <99056561+victorcaetanos@users.noreply.github.com>
2024-12-05 21:10:34 -0300

    Fixeed unnecessary getAll on component loading

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -8,48 +8,35 @@ const Comments = ({ id_aluno: idAluno }) => {
     const urlComment = "http://localhost:4000/api/comments";
 
     useEffect(() => {
-        const fetchComments = async () => {
-            try {
-                const response = await axios.get(`${urlComment}/${idAluno}`);
-                console.log(response);
-                if (response.status !== 200) {
-                    throw new Error(response.data);
-                }
-                setListaComments(Array.isArray(response.data) ? response.data : [response.data]);
-                setLoading(false);
-            } catch (err) {
-                setError(err.response ? err.response.data : err.message);
-                setLoading(false);
-            }
-        };
-
-        fetchComments();
-    }, [idAluno]); 
+        buscarComentarios();
+    }, [idAluno]);
 
     async function buscarComentarios() {
         try {
-            const response = await axios.get(urlComment);
-            setListaComments(response.data);
-        } catch (error) {
-            console.error('Erro ao buscar Comentários:', error);
+            const response = await axios.get(`${urlComment}/${idAluno}`);
+            console.log(response);
+            if (response.status !== 200) {
+                throw new Error(response.data);
+            }
+            setListaComments(Array.isArray(response.data) ? response.data : [response.data]);
+            setLoading(false);
+        } catch (err) {
+            setError(err.response ? err.response.data : err.message);
+            setLoading(false);
         }
     }
 
     function removerComentario(idComentario) {
-      axios
-        .delete(`${urlComment}/${idComentario}`)
-        .then(() => {
-          alert("Comentario deletado com sucesso:");
-        })
-        .catch((error) => {
-          console.error("Erro ao deletar comentario:", error);
-        });
-      buscarComentarios();
-    }
-
-    useEffect(() => {
+        axios
+            .delete(`${urlComment}/${idComentario}`)
+            .then(() => {
+                alert("Comentario deletado com sucesso:");
+            })
+            .catch((error) => {
+                console.error("Erro ao deletar comentario:", error);
+            });
         buscarComentarios();
-    }, []);
+    }
 
     if (loading) return <div>Procurando comentários...</div>;
     if (error) return <div>Erro: {error}</div>;
